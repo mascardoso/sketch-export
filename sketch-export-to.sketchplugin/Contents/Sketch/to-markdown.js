d689c47764e3702e5f7eb3ca2e4a85df6a523d06
Marco Cardoso <mascardoso@gmail.com>
2018-11-22 12:12:01 +0100

    added new babel dependencies for async and await

--- sketch-export-to.sketchplugin/Contents/Sketch/to-markdown.js
+++ sketch-export-to.sketchplugin/Contents/Sketch/to-markdown.js
@@ -91,414 +91,6 @@ var exports =
 /************************************************************************/
 /******/ ({
 
-/***/ "./node_modules/@skpm/fs/index.js":
-/*!****************************************!*\
-  !*** ./node_modules/@skpm/fs/index.js ***!
-  \****************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-// TODO: async. Should probably be done with NSFileHandle and some notifications
-// TODO: file descriptor. Needs to be done with NSFileHandle
-var Buffer = __webpack_require__(/*! buffer */ "buffer").Buffer
-
-function encodingFromOptions(options, defaultValue) {
-  return options && options.encoding
-    ? String(options.encoding)
-    : (
-      options
-        ? String(options)
-        : defaultValue
-    )
-}
-
-module.exports.constants = {
-  F_OK: 0,
-  R_OK: 4,
-  W_OK: 2,
-  X_OK: 1
-}
-
-module.exports.accessSync = function(path, mode) {
-  mode = mode | 0
-  var fileManager = NSFileManager.defaultManager()
-
-  switch (mode) {
-    case 0:
-      canAccess = module.exports.existsSync(path)
-      break
-    case 1:
-      canAccess = Boolean(Number(fileManager.isExecutableFileAtPath(path)))
-      break
-    case 2:
-      canAccess = Boolean(Number(fileManager.isWritableFileAtPath(path)))
-      break
-    case 3:
-      canAccess = Boolean(Number(fileManager.isExecutableFileAtPath(path))) && Boolean(Number(fileManager.isWritableFileAtPath(path)))
-      break
-    case 4:
-      canAccess = Boolean(Number(fileManager.isReadableFileAtPath(path)))
-      break
-    case 5:
-      canAccess = Boolean(Number(fileManager.isReadableFileAtPath(path))) && Boolean(Number(fileManager.isExecutableFileAtPath(path)))
-      break
-    case 6:
-      canAccess = Boolean(Number(fileManager.isReadableFileAtPath(path))) && Boolean(Number(fileManager.isWritableFileAtPath(path)))
-      break
-    case 7:
-      canAccess = Boolean(Number(fileManager.isReadableFileAtPath(path))) && Boolean(Number(fileManager.isWritableFileAtPath(path))) && Boolean(Number(fileManager.isExecutableFileAtPath(path)))
-      break
-  }
-
-  if (!canAccess) {
-    throw new Error('Can\'t access ' + String(path))
-  }
-}
-
-module.exports.appendFileSync = function(file, data, options) {
-  if (!module.exports.existsSync(file)) {
-    return module.exports.writeFileSync(file, data, options)
-  }
-
-  var handle = NSFileHandle.fileHandleForWritingAtPath(file)
-  handle.seekToEndOfFile()
-
-  var encoding = encodingFromOptions(options, 'utf8')
-
-  var nsdata = Buffer.from(data, encoding === 'NSData' || encoding === 'buffer' ? undefined : encoding).toNSData()
-
-  handle.writeData(nsdata)
-}
-
-module.exports.chmodSync = function(path, mode) {
-  var err = MOPointer.alloc().init()
-  var fileManager = NSFileManager.defaultManager()
-  fileManager.setAttributes_ofItemAtPath_error({
-    NSFilePosixPermissions: mode
-  }, path, err)
-
-  if (err.value() !== null) {
-    throw new Error(err.value())
-  }
-}
-
-module.exports.copyFileSync = function(path, dest, flags) {
-  var err = MOPointer.alloc().init()
-  var fileManager = NSFileManager.defaultManager()
-  fileManager.copyItemAtPath_toPath_error(path, dest, err)
-
-  if (err.value() !== null) {
-    throw new Error(err.value())
-  }
-}
-
-module.exports.existsSync = function(path) {
-  var fileManager = NSFileManager.defaultManager()
-  return Boolean(Number(fileManager.fileExistsAtPath(path)))
-}
-
-module.exports.linkSync = function(existingPath, newPath) {
-  var err = MOPointer.alloc().init()
-  var fileManager = NSFileManager.defaultManager()
-  fileManager.linkItemAtPath_toPath_error(existingPath, newPath, err)
-
-  if (err.value() !== null) {
-    throw new Error(err.value())
-  }
-}
-
-module.exports.mkdirSync = function(path, mode) {
-  mode = mode || 0o777
-  var err = MOPointer.alloc().init()
-  var fileManager = NSFileManager.defaultManager()
-  fileManager.createDirectoryAtPath_withIntermediateDirectories_attributes_error(path, false, {
-    NSFilePosixPermissions: mode
-  }, err)
-
-  if (err.value() !== null) {
-    throw new Error(err.value())
-  }
-}
-
-module.exports.mkdtempSync = function(path) {
-  function makeid() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (var i = 0; i < 6; i++)
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    return text;
-  }
-  var tempPath = path + makeid()
-  module.exports.mkdirSync(tempPath)
-  return tempPath
-}
-
-module.exports.readdirSync = function(path) {
-  var fileManager = NSFileManager.defaultManager()
-  var paths = fileManager.subpathsAtPath(path)
-  var arr = []
-  for (var i = 0; i < paths.length; i++) {
-    arr.push(String(paths[i]))
-  }
-  return arr
-}
-
-module.exports.readFileSync = function(path, options) {
-  var encoding = encodingFromOptions(options, 'buffer')
-  var fileManager = NSFileManager.defaultManager()
-  var data = fileManager.contentsAtPath(path)
-  var buffer = Buffer.from(data)
-
-  if (encoding === 'buffer') {
-    return buffer
-  } else if (encoding === 'NSData') {
-    return buffer.toNSData()
-  } else {
-    return buffer.toString(encoding)
-  }
-}
-
-module.exports.readlinkSync = function(path) {
-  var err = MOPointer.alloc().init()
-  var fileManager = NSFileManager.defaultManager()
-  var result = fileManager.destinationOfSymbolicLinkAtPath_error(path, err)
-
-  if (err.value() !== null) {
-    throw new Error(err.value())
-  }
-
-  return String(result)
-}
-
-module.exports.realpathSync = function(path) {
-  return String(NSString.stringByResolvingSymlinksInPath(path))
-}
-
-module.exports.renameSync = function(oldPath, newPath) {
-  var err = MOPointer.alloc().init()
-  var fileManager = NSFileManager.defaultManager()
-  fileManager.moveItemAtPath_toPath_error(oldPath, newPath, err)
-
-  if (err.value() !== null) {
-    throw new Error(err.value())
-  }
-}
-
-module.exports.rmdirSync = function(path) {
-  var err = MOPointer.alloc().init()
-  var fileManager = NSFileManager.defaultManager()
-  fileManager.removeItemAtPath_error(path, err)
-
-  if (err.value() !== null) {
-    throw new Error(err.value())
-  }
-}
-
-module.exports.statSync = function(path) {
-  var err = MOPointer.alloc().init()
-  var fileManager = NSFileManager.defaultManager()
-  var result = fileManager.attributesOfItemAtPath_error(path, err)
-
-  if (err.value() !== null) {
-    throw new Error(err.value())
-  }
-
-  return {
-    dev: String(result.NSFileDeviceIdentifier),
-    // ino: 48064969, The file system specific "Inode" number for the file.
-    mode: result.NSFileType | result.NSFilePosixPermissions,
-    nlink: Number(result.NSFileReferenceCount),
-    uid: String(result.NSFileOwnerAccountID),
-    gid: String(result.NSFileGroupOwnerAccountID),
-    // rdev: 0, A numeric device identifier if the file is considered "special".
-    size: Number(result.NSFileSize),
-    // blksize: 4096, The file system block size for i/o operations.
-    // blocks: 8, The number of blocks allocated for this file.
-    atimeMs: Number(result.NSFileModificationDate.timeIntervalSince1970()) * 1000,
-    mtimeMs: Number(result.NSFileModificationDate.timeIntervalSince1970()) * 1000,
-    ctimeMs: Number(result.NSFileModificationDate.timeIntervalSince1970()) * 1000,
-    birthtimeMs: Number(result.NSFileCreationDate.timeIntervalSince1970()) * 1000,
-    atime: new Date(Number(result.NSFileModificationDate.timeIntervalSince1970()) * 1000 + 0.5), // the 0.5 comes from the node source. Not sure why it's added but in doubt...
-    mtime: new Date(Number(result.NSFileModificationDate.timeIntervalSince1970()) * 1000 + 0.5),
-    ctime: new Date(Number(result.NSFileModificationDate.timeIntervalSince1970()) * 1000 + 0.5),
-    birthtime: new Date(Number(result.NSFileCreationDate.timeIntervalSince1970()) * 1000 + 0.5),
-    isBlockDevice: function() { return result.NSFileType === NSFileTypeBlockSpecial },
-    isCharacterDevice: function() { return result.NSFileType === NSFileTypeCharacterSpecial },
-    isDirectory: function() { return result.NSFileType === NSFileTypeDirectory },
-    isFIFO: function() { return false },
-    isFile: function() { return result.NSFileType === NSFileTypeRegular },
-    isSocket: function() { return result.NSFileType === NSFileTypeSocket },
-    isSymbolicLink: function() { return result.NSFileType === NSFileTypeSymbolicLink },
-  }
-}
-
-module.exports.symlinkSync = function(target, path) {
-  var err = MOPointer.alloc().init()
-  var fileManager = NSFileManager.defaultManager()
-  var result = fileManager.createSymbolicLinkAtPath_withDestinationPath_error(path, target, err)
-
-  if (err.value() !== null) {
-    throw new Error(err.value())
-  }
-}
-
-module.exports.truncateSync = function(path, len) {
-  var hFile = NSFileHandle.fileHandleForUpdatingAtPath(sFilePath)
-  hFile.truncateFileAtOffset(len || 0)
-  hFile.closeFile()
-}
-
-module.exports.unlinkSync = function(path) {
-  var err = MOPointer.alloc().init()
-  var fileManager = NSFileManager.defaultManager()
-  var result = fileManager.removeItemAtPath_error(path, err)
-
-  if (err.value() !== null) {
-    throw new Error(err.value())
-  }
-}
-
-module.exports.utimesSync = function(path, aTime, mTime) {
-  var err = MOPointer.alloc().init()
-  var fileManager = NSFileManager.defaultManager()
-  var result = fileManager.setAttributes_ofItemAtPath_error({
-    NSFileModificationDate: aTime
-  }, path, err)
-
-  if (err.value() !== null) {
-    throw new Error(err.value())
-  }
-}
-
-module.exports.writeFileSync = function(path, data, options) {
-  var encoding = encodingFromOptions(options, 'utf8')
-
-  var nsdata = Buffer.from(
-    data, encoding === 'NSData' || encoding === 'buffer' ? undefined : encoding
-  ).toNSData()
-
-  nsdata.writeToFile_atomically(path, true)
-}
-
-
-/***/ }),
-
-/***/ "./src/parse-layers.js":
-/*!*****************************!*\
-  !*** ./src/parse-layers.js ***!
-  \*****************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-var sketchDom = __webpack_require__(/*! sketch/dom */ "sketch/dom");
-
-var md = "";
-var imgRegex = /^image-/;
-
-var getFontName = function getFontName(layer) {
-  return layer.sketchObject.style().primitiveTextStyle().attributes()["NSFont"].fontName();
-};
-
-var isBold = function isBold(layer) {
-  return getFontName(layer).match(/(Bold|bold)$/);
-};
-
-var isItalic = function isItalic(layer) {
-  return getFontName(layer).match(/(Italic|italic|Oblique|oblique)$/);
-};
-
-var getFontWeight = function getFontWeight(layer) {
-  if (isBold(layer)) {
-    return "**";
-  } else if (isItalic(layer)) {
-    return "*";
-  } else {
-    return "";
-  }
-};
-
-var processLayers = function processLayers(layer, directoryPath) {
-  // process groups but ignore images group layers or symbols
-  if (layer.type === "Group" && !layer.name.match(imgRegex)) {
-    layer.layers.reverse().map(function (layer) {
-      processLayers(layer);
-    });
-  } else {
-    parseToMd(layer.name, layer, directoryPath);
-  }
-};
-
-var readLayers = function readLayers(allLayers, artboardName, directoryPath) {
-  allLayers.map(function (layer) {
-    if (layer.type === "Artboard" && layer.name === artboardName) {
-      layer.layers.reverse().map(function (layer) {
-        processLayers(layer, directoryPath);
-      });
-    }
-  });
-};
-
-var parseToMd = function parseToMd(layerName, layer, directoryPath) {
-  // if layer starts with image* set the layerName to image
-  layerName = layerName.match(imgRegex) ? "image" : layerName;
-
-  switch (layerName) {
-    case "heading1":
-      md += "# ".concat(layer.text.trim(), "\n");
-      break;
-
-    case "heading2":
-      md += "## ".concat(layer.text.trim(), "\n");
-      break;
-
-    case "heading3":
-      md += "### ".concat(layer.text.trim(), "\n");
-      break;
-
-    case "heading4":
-      md += "#### ".concat(layer.text.trim(), "\n");
-      break;
-
-    case "image":
-      sketchDom.export(layer, {
-        formats: "jpg",
-        output: directoryPath,
-        overwriting: true,
-        scales: "1"
-      });
-      md += "![](./".concat(layer.name, ".jpg)\n\n");
-      break;
-
-    case "list":
-      layer.text.trim().split("\n").forEach(function (listItem, key, content) {
-        md += "* ".concat(listItem, "\n").concat(Object.is(content.length - 1, key) ? "\n" : "");
-      });
-      break;
-
-    case "paragraph-multi":
-      layer.text.trim().split("\n").forEach(function (paragraph, key, content) {
-        md += "".concat(getFontWeight(layer)).concat(paragraph).concat(getFontWeight(layer), "\n").concat(Object.is(content.length - 1, key) ? "\n" : "");
-      });
-      break;
-
-    case "paragraph":
-      md += "".concat(getFontWeight(layer)).concat(layer.text.trim()).concat(getFontWeight(layer), "\n\n");
-  }
-};
-
-var getMdContent = function getMdContent(allLayers, artboardName, directoryPath) {
-  readLayers(allLayers, artboardName, directoryPath);
-  return md;
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (getMdContent);
-
-/***/ }),
-
 /***/ "./src/to-markdown.js":
 /*!****************************!*\
   !*** ./src/to-markdown.js ***!
@@ -508,22 +100,59 @@ var getMdContent = function getMdContent(allLayers, artboardName, directoryPath)
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _skpm_fs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @skpm/fs */ "./node_modules/@skpm/fs/index.js");
-/* harmony import */ var _skpm_fs__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_skpm_fs__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _parse_layers__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./parse-layers */ "./src/parse-layers.js");
-
-
-
-var document = __webpack_require__(/*! sketch/dom */ "sketch/dom").getSelectedDocument();
-
+// import fs from "@skpm/fs";
+// // import getMdContent from "./parse-layers";
 var UI = __webpack_require__(/*! sketch/ui */ "sketch/ui");
 
-var saveMd = function saveMd(path, docName, artboardName, content) {
-  _skpm_fs__WEBPACK_IMPORTED_MODULE_0___default.a.writeFileSync("".concat(path).concat(docName, "-").concat(artboardName, ".md"), content, "utf8");
-};
+var sketchDom = __webpack_require__(/*! sketch/dom */ "sketch/dom"); // const saveMd = (path, docName, artboardName, content) => {
+//   fs.writeFileSync(`${path}${docName}-${artboardName}.md`, content, "utf8");
+// };
+// export default function() {
+//   const docName = document.sketchObject.displayName().replace(".sketch", ""); // remove sketch extension
+//   const directoryPath = document.path.replace(
+//     document.sketchObject.displayName(),
+//     ""
+//   ); // remove filename
+//   const page = document.selectedPage;
+//   const allLayers = page.layers;
+//   let artboards = [];
+//   allLayers.forEach(layer => {
+//     if (layer.type === "Artboard") {
+//       artboards.push(layer.name);
+//     }
+//   });
+//   if (artboards.length === 0) {
+//     UI.message("You have no artboards in your page. You need at least one.");
+//   } else {
+//     const selection = UI.getSelectionFromUser(
+//       "Which artboard you want to export to markdown",
+//       artboards.reverse()
+//     );
+//     const ok = selection[2];
+//     const selectedArtboard = artboards[selection[1]];
+//     if (ok) {
+//       try {
+//         // saveMd(
+//         //   directoryPath,
+//         //   docName,
+//         //   selectedArtboard,
+//         //   getMdContent(allLayers, selectedArtboard, directoryPath)
+//         // );
+//         UI.message(
+//           `🎉 ${selectedArtboard} was successfully exported to markdown 🎉`
+//         );
+//       } catch (err) {
+//         UI.message(`${err}. Try again.`);
+//       }
+//     }
+//   }
+// }
 
-/* harmony default export */ __webpack_exports__["default"] = (function () {
-  var docName = document.sketchObject.displayName().replace(".sketch", ""); // remove sketch extension
+
+/* harmony default export */ __webpack_exports__["default"] = (function (context) {
+  // const document = context.getSelectedDocument();
+  var document = sketchDom.fromNative(context.document);
+  var fileName = document.sketchObject.displayName().replace(".sketch", ""); // remove sketch extension
 
   var directoryPath = document.path.replace(document.sketchObject.displayName(), ""); // remove filename
 
@@ -545,25 +174,40 @@ var saveMd = function saveMd(path, docName, artboardName, content) {
 
     if (ok) {
       try {
-        saveMd(directoryPath, docName, selectedArtboard, Object(_parse_layers__WEBPACK_IMPORTED_MODULE_1__["default"])(allLayers, selectedArtboard, directoryPath));
+        // saveMd(
+        //   directoryPath,
+        //   docName,
+        //   selectedArtboard,
+        //   getMdContent(allLayers, selectedArtboard, directoryPath)
+        // );
         UI.message("\uD83C\uDF89 ".concat(selectedArtboard, " was successfully exported to markdown \uD83C\uDF89"));
       } catch (err) {
         UI.message("".concat(err, ". Try again."));
       }
     }
-  }
+  } // const selectedLayers = context.selection;
+  // const selectedCount = selectedLayers.count();
+  // /* Make sure there is at least 1 layer to fill. */
+  // if (!selectedCount) {
+  //   context.document.showMessage(
+  //     "❌ Cannot perform Bacon Ipsum - No layers selected!"
+  //   );
+  //   log("❌ Failed - No layers selected!");
+  //   return false;
+  // }
+  // /* Fetch the 'Bacon ipsum' paragraph. */
+  // const request = await fetch(
+  //   `https://baconipsum.com/api/?type=all-meat&paras=${selectedCount}&start-with-lorem=0`
+  // );
+  // const baconIpsum = await request.json();
+  // /* Fill each layer with 'Bacon ipsum ...' */
+  // selectedLayers.forEach((layer, index) => {
+  //   layer.stringValue = baconIpsum[index];
+  // });
+  // context.document.showMessage("🥓 Enjoy your bacon! 🥓");
+  // log("✅ Complete!");
+
 });
-
-/***/ }),
-
-/***/ "buffer":
-/*!*************************!*\
-  !*** external "buffer" ***!
-  \*************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-module.exports = require("buffer");
 
 /***/ }),
 
--- src/to-markdown.js
+++ src/to-markdown.js
@@ -1,15 +1,60 @@
-import fs from "@skpm/fs";
-import getMdContent from "./parse-layers";
+// import fs from "@skpm/fs";
+// // import getMdContent from "./parse-layers";
 
-const document = require("sketch/dom").getSelectedDocument();
 const UI = require("sketch/ui");
+const sketchDom = require("sketch/dom");
 
-const saveMd = (path, docName, artboardName, content) => {
-  fs.writeFileSync(`${path}${docName}-${artboardName}.md`, content, "utf8");
-};
+// const saveMd = (path, docName, artboardName, content) => {
+//   fs.writeFileSync(`${path}${docName}-${artboardName}.md`, content, "utf8");
+// };
 
-export default function() {
-  const docName = document.sketchObject.displayName().replace(".sketch", ""); // remove sketch extension
+// export default function() {
+//   const docName = document.sketchObject.displayName().replace(".sketch", ""); // remove sketch extension
+//   const directoryPath = document.path.replace(
+//     document.sketchObject.displayName(),
+//     ""
+//   ); // remove filename
+//   const page = document.selectedPage;
+//   const allLayers = page.layers;
+
+//   let artboards = [];
+
+//   allLayers.forEach(layer => {
+//     if (layer.type === "Artboard") {
+//       artboards.push(layer.name);
+//     }
+//   });
+
+//   if (artboards.length === 0) {
+//     UI.message("You have no artboards in your page. You need at least one.");
+//   } else {
+//     const selection = UI.getSelectionFromUser(
+//       "Which artboard you want to export to markdown",
+//       artboards.reverse()
+//     );
+//     const ok = selection[2];
+//     const selectedArtboard = artboards[selection[1]];
+//     if (ok) {
+//       try {
+//         // saveMd(
+//         //   directoryPath,
+//         //   docName,
+//         //   selectedArtboard,
+//         //   getMdContent(allLayers, selectedArtboard, directoryPath)
+//         // );
+//         UI.message(
+//           `🎉 ${selectedArtboard} was successfully exported to markdown 🎉`
+//         );
+//       } catch (err) {
+//         UI.message(`${err}. Try again.`);
+//       }
+//     }
+//   }
+// }
+export default context => {
+  // const document = context.getSelectedDocument();
+  const document = sketchDom.fromNative(context.document);
+  const fileName = document.sketchObject.displayName().replace(".sketch", ""); // remove sketch extension
   const directoryPath = document.path.replace(
     document.sketchObject.displayName(),
     ""
@@ -36,12 +81,12 @@ export default function() {
     const selectedArtboard = artboards[selection[1]];
     if (ok) {
       try {
-        saveMd(
-          directoryPath,
-          docName,
-          selectedArtboard,
-          getMdContent(allLayers, selectedArtboard, directoryPath)
-        );
+        // saveMd(
+        //   directoryPath,
+        //   docName,
+        //   selectedArtboard,
+        //   getMdContent(allLayers, selectedArtboard, directoryPath)
+        // );
         UI.message(
           `🎉 ${selectedArtboard} was successfully exported to markdown 🎉`
         );
@@ -50,4 +95,30 @@ export default function() {
       }
     }
   }
-}
+
+  // const selectedLayers = context.selection;
+  // const selectedCount = selectedLayers.count();
+
+  // /* Make sure there is at least 1 layer to fill. */
+  // if (!selectedCount) {
+  //   context.document.showMessage(
+  //     "❌ Cannot perform Bacon Ipsum - No layers selected!"
+  //   );
+  //   log("❌ Failed - No layers selected!");
+  //   return false;
+  // }
+
+  // /* Fetch the 'Bacon ipsum' paragraph. */
+  // const request = await fetch(
+  //   `https://baconipsum.com/api/?type=all-meat&paras=${selectedCount}&start-with-lorem=0`
+  // );
+  // const baconIpsum = await request.json();
+
+  // /* Fill each layer with 'Bacon ipsum ...' */
+  // selectedLayers.forEach((layer, index) => {
+  //   layer.stringValue = baconIpsum[index];
+  // });
+
+  // context.document.showMessage("🥓 Enjoy your bacon! 🥓");
+  // log("✅ Complete!");
+};
